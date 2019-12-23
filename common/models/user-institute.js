2e83212c93655c9a2259f4bbd754db5d95e07eaa
anas-1995 <anasalazmeh.95@gmail.com>
2019-12-23 16:36:11 +0200

    property solved

--- common/models/user-institute.js
+++ common/models/user-institute.js
@@ -1,6 +1,7 @@
 'use strict';
 var g = require('strong-globalize')();
 var debug = require('debug')('loopback:user');
+var async = require("async");
 
 module.exports = function (Userinstitute) {
 
@@ -51,4 +52,37 @@ module.exports = function (Userinstitute) {
       callback(error)
     }
   };
+
+  Userinstitute.getAllBranch = async function (req, callback) {
+    try {
+      var principalType = req.accessToken.principalType
+      var mainUserId = req.accessToken.mainUserId
+      if (principalType != "userInstitute") {
+        throw Userinstitute.app.err.global.authorization()
+      }
+      var user = await Userinstitute.findById(mainUserId);
+
+      var allUserBranch = []
+      var userInstitute = await Userinstitute.app.models.instituteAdmin.find({
+        "where": {
+          "userInstituteId": user.id
+        }
+      })
+
+      for (let index = 0; index < userInstitute.length; index++) {
+        const element = userInstitute[index];
+        const brnach = await Userinstitute.app.models.Branch.find({
+          "where": {
+            "instituteId": element.instituteId
+          }
+        })
+        allUserBranch = allUserBranch.concat(brnach)
+        if (index + 1 == userInstitute.length) {
+          callback(null, allUserBranch)
+        }
+      }
+    } catch (error) {
+      callback(error)
+    }
+  }
 };
