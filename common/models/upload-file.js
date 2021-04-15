2886cbb67843dff0f7f628bc2411416a67d43c25
anas-1995 <anasalazmeh.95@gmail.com>
2021-04-15 15:46:35 +0300

    test thumb

--- common/models/upload-file.js
+++ common/models/upload-file.js
@@ -1,99 +1,144 @@
 'use strict';
 const path = require('path');
 const configPath = process.env.NODE_ENV === undefined ?
-  '../../server/config.json' :
-  `../../server/config.${process.env.NODE_ENV}.json`;
+    '../../server/config.json' :
+    `../../server/config.${process.env.NODE_ENV}.json`;
 const config = require(configPath);
 var ffmpeg = require('fluent-ffmpeg');
 const { getVideoDurationInSeconds } = require('get-video-duration');
 const {
-  getAudioDurationInSeconds
+    getAudioDurationInSeconds
 } = require('get-audio-duration')
 
-module.exports = function (Uploadfile) {
+module.exports = function(Uploadfile) {
 
 
-  Uploadfile.afterRemote('upload', function (context, result, next) {
-    // console.log(context.req.accessToken)
-    let userId = context.req.accessToken.userId
-    let files = [];
-    var folderName = context.req.params.container;
-    let src = path.join(__dirname, '../../../MGMTImage/uploadFiles/');
-    var urlFileRoot = config.domain + config.restApiRoot + Uploadfile.http.path;
-    var urlFileRootSave = urlFileRoot + '/' + folderName + '/download/'
-    var urlThumbRootSave = urlFileRoot + '/' + "thumbnail" + '/download/'
-    var urlWebThumbRootSave = urlFileRoot + '/' + "web-thumbnail" + '/download/'
+    Uploadfile.afterRemote('upload', function(context, result, next) {
+        // console.log(context.req.accessToken)
+        let userId
+        if (context.req.accessToken)
+            userId = context.req.accessToken.userId
+        let files = [];
+        var folderName = context.req.params.container;
+        let src = path.join(__dirname, '../../../MGMTImage/uploadFiles/');
+        var urlFileRoot = config.domain + config.restApiRoot + Uploadfile.http.path;
+        var urlFileRootSave = urlFileRoot + '/' + folderName + '/download/'
+        var urlThumbRootSave = urlFileRoot + '/' + "thumbnail" + '/download/'
+        var urlWebThumbRootSave = urlFileRoot + '/' + "web-thumbnail" + '/download/'
 
-    if (process.env.NODE_ENV != undefined) {
-      ffmpeg.setFfmpegPath(path.join(config.thumbUrl + config.programFFmpegName[0]));
-      ffmpeg.setFfprobePath(path.join(config.thumbUrl + config.programFFmpegName[1]));
-    }
-
-
-
-    let arrayFiles = result.result.files.file;
-    for (let index = 0; index < arrayFiles.length; index++) {
-      const file = arrayFiles[index];
-      if (folderName == "video") {
-
-        getVideoDurationInSeconds(src + "/video/" + file.name).then((duration) => {
-          console.log(duration)
-          files.push({
-            'url': urlFileRootSave + file.name,
-            'duration': duration,
-            'type': folderName,
-            "userId": userId,
-            'thumb': "", //urlThumbRootSave + file.name.substring(0, file.name.lastIndexOf('.')) + "_thumb." + extension,
-            'webThumbUrl': "" //urlWebThumbRootSave + file.name.substring(0, file.name.lastIndexOf('.')) + "_thumb." + extension
-          });
-          if (arrayFiles.length == index + 1) {
-            Uploadfile.app.models.media.create(files, function (err, data) {
-              if (err)
-                return next(err)
-              context.res.json(data);
-            })
-          }
-        })
-      } else if (folderName == "audio") {
-        getAudioDurationInSeconds(src + "/audio/" + file.name).then((duration) => {
-          console.log(duration)
-          files.push({
-            'url': urlFileRootSave + file.name,
-            'duration': duration,
-            'type': folderName,
-            "userId": userId,
-            'thumb': "", //urlThumbRootSave + file.name.substring(0, file.name.lastIndexOf('.')) + "_thumb." + extension,
-            'webThumbUrl': "" //urlWebThumbRootSave + file.name.substring(0, file.name.lastIndexOf('.')) + "_thumb." + extension
-          });
-          if (arrayFiles.length == index + 1) {
-            Uploadfile.app.models.media.create(files, function (err, data) {
-              if (err)
-                return next(err)
-              context.res.json(data);
-            })
-          }
-        })
-      } else {
-        files.push({
-          'url': urlFileRootSave + file.name,
-          'type': folderName,
-          "userId": userId,
-          'thumb': "", //urlThumbRootSave + file.name.substring(0, file.name.lastIndexOf('.')) + "_thumb." + extension,
-          'webThumbUrl': "" //urlWebThumbRootSave + file.name.substring(0, file.name.lastIndexOf('.')) + "_thumb." + extension
-        });
-        if (arrayFiles.length == index + 1) {
-          Uploadfile.app.models.media.create(files, function (err, data) {
-            if (err)
-              return next(err)
-            context.res.json(data);
-          })
+        if (process.env.NODE_ENV != undefined) {
+            ffmpeg.setFfmpegPath(path.join(config.thumbUrl + config.programFFmpegName[0]));
+            ffmpeg.setFfprobePath(path.join(config.thumbUrl + config.programFFmpegName[1]));
         }
-      }
-    }
 
 
 
-    console.log(files)
+        let arrayFiles = result.result.files.file;
+        for (let index = 0; index < arrayFiles.length; index++) {
+            const file = arrayFiles[index];
+            if (folderName == "video") {
 
-  })
+                getVideoDurationInSeconds(src + "/video/" + file.name).then((duration) => {
+                    var newWidth = 0
+                    var newHeight = 0
+                    var oldWidth = 0;
+                    var oldHeight = 0;
+                    var rotation;
+                    var size
+                    ffmpeg.ffprobe(src + "/" + folderName + "/" + file.name, function(err, metadata) {
+                        // ffmpeg.ffprobe("P:/vibo/VobbleApi/uploadFiles/videos/5efa41f0-db2a-11ea-b3fb-b7d2915714b61597078561678.mp4", function (err, metadata) {
+                        if (err) { console.log(err);
+                            console.log("err") } else {
+                            //console.log(metadata);
+                            metadata['streams'].forEach(function(element) {
+                                if (element.width) {
+                                    oldWidth = element.width;
+                                    oldHeight = element.height;
+                                    rotation = element.rotation
+                                }
+                            }, this);
+                            if (oldWidth != 0)
+                                var res = oldWidth / oldHeight;
+                            else
+                                var res = 2
+                                    //console.log("res");
+                                    //console.log(res);
+                            if (rotation == -90 || rotation == 90) {
+                                newHeight = 400
+                                newWidth = newHeight * res
+                                size = newHeight + 'x' + parseInt(newWidth)
+                            } else {
+                                newWidth = 400
+                                newHeight = newWidth / res
+                                size = newWidth + 'x' + parseInt(newHeight)
+                            }
+
+                            ffmpeg(src + "/" + folderName + "/" + file.name)
+                                .screenshot({
+                                    count: 1,
+                                    filename: file.name.substring(0, file.name.lastIndexOf('.')) + "_thumb.PNG",
+                                    folder: src + '/thumbnail/',
+                                    size: size
+                                });
+
+                            files.push({
+                                'url': urlFileRootSave + file.name,
+                                'duration': duration,
+                                'type': folderName,
+                                "userId": userId,
+                                'thumb': urlThumbRootSave + file.name.substring(0, file.name.lastIndexOf('.')) + "_thumb." + "png", //extension,
+                                'webThumbUrl': "" //urlWebThumbRootSave + file.name.substring(0, file.name.lastIndexOf('.')) + "_thumb." + extension
+                            });
+                            if (arrayFiles.length == index + 1) {
+                                Uploadfile.app.models.media.create(files, function(err, data) {
+                                    if (err)
+                                        return next(err)
+                                    context.res.json(data);
+                                })
+                            }
+                        }
+                    })
+                })
+            } else if (folderName == "audio") {
+                getAudioDurationInSeconds(src + "/audio/" + file.name).then((duration) => {
+                    console.log(duration)
+                    files.push({
+                        'url': urlFileRootSave + file.name,
+                        'duration': duration,
+                        'type': folderName,
+                        "userId": userId,
+                        'thumb': "", //urlThumbRootSave + file.name.substring(0, file.name.lastIndexOf('.')) + "_thumb." + extension,
+                        'webThumbUrl': "" //urlWebThumbRootSave + file.name.substring(0, file.name.lastIndexOf('.')) + "_thumb." + extension
+                    });
+                    if (arrayFiles.length == index + 1) {
+                        Uploadfile.app.models.media.create(files, function(err, data) {
+                            if (err)
+                                return next(err)
+                            context.res.json(data);
+                        })
+                    }
+                })
+            } else {
+                files.push({
+                    'url': urlFileRootSave + file.name,
+                    'type': folderName,
+                    "userId": userId,
+                    'thumb': "", //urlThumbRootSave + file.name.substring(0, file.name.lastIndexOf('.')) + "_thumb." + extension,
+                    'webThumbUrl': "" //urlWebThumbRootSave + file.name.substring(0, file.name.lastIndexOf('.')) + "_thumb." + extension
+                });
+                if (arrayFiles.length == index + 1) {
+                    Uploadfile.app.models.media.create(files, function(err, data) {
+                        if (err)
+                            return next(err)
+                        context.res.json(data);
+                    })
+                }
+            }
+        }
+
+
+
+        console.log(files)
+
+    })
 };
