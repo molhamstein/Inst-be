ae5b458d0a6f347268a0e3a2a6086640612e2f48
anas-1995 <anasalazmeh.95@gmail.com>
2021-06-03 10:35:01 +0300

    add ssl

--- server/server.js
+++ server/server.js
@@ -5,52 +5,54 @@ const service = require('./boot/service.js');
 console.log(service)
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var loopbackSSL = require('loopback-ssl');
 
 var app = module.exports = loopback();
 app.use(loopback.token({
-  model: app.models.MultiAccessToken
+    model: app.models.MultiAccessToken
 }));
 
-app.use(async function (req, res, next) {
-  if (req.accessToken) {
-    console.log("has accesstoken")
-    console.log(req.accessToken.principalType)
-    var principalType = req.accessToken.principalType
-    var userId = req.accessToken.userId
-    if (principalType == "userInstitute") {
-      req.accessToken.mainUserId = userId
-      next()
+app.use(async function(req, res, next) {
+    if (req.accessToken) {
+        console.log("has accesstoken")
+        console.log(req.accessToken.principalType)
+        var principalType = req.accessToken.principalType
+        var userId = req.accessToken.userId
+        if (principalType == "userInstitute") {
+            req.accessToken.mainUserId = userId
+            next()
+        } else {
+            next()
+        }
     } else {
-      next()
+        next()
     }
-  } else {
-    next()
-  }
 });
 
 
 app.query = query;
 app.err = errors;
 app.service = service;
-app.start = function () {
-  // start the web server
-  return app.listen(function () {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
+app.start = function() {
+    // start the web server
+    return app.listen(function() {
+        app.emit('started');
+        var baseUrl = app.get('url').replace(/\/$/, '');
+        console.log('Web server listening at: %s', baseUrl);
+        if (app.get('loopback-component-explorer')) {
+            var explorerPath = app.get('loopback-component-explorer').mountPath;
+            console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+        }
+    });
 };
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function (err) {
-  if (err) throw err;
+boot(app, __dirname, function(err) {
+    if (err) throw err;
 
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+    // start the server if `$ node server.js`
+    // if (require.main === module)
+    //   app.start();
+    return loopbackSSL.startServer(app);
 });
