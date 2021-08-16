d17b39bc2bcb22c596d2892622f43bc081f0d48a
anas-1995 <anasalazmeh.95@gmail.com>
2021-08-16 17:01:28 +0300

    solve path firebase

--- common/models/notification.js
+++ common/models/notification.js
@@ -62,7 +62,7 @@ module.exports = function(Notification) {
 
         for (let index = 0; index < appNotificationData.length; index++) {
             const element = appNotificationData[index];
-            let data = { "notificationId": element.id.toString(), type: typeObject.name };
+            let data = { "click_action": "FLUTTER_NOTIFICATION_CLICK", "notificationId": element.id.toString(), type: typeObject.name };
             if (element['courseId']) {
                 data['courseId'] = element['courseId'].toString()
             }
